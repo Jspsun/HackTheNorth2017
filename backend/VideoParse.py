a1b9a841f6864bd7f048eaf8f6945a950587e7a1
aidenbenner <aiden.benner@gmail.com>
2017-09-16 03:47:12 -0400

    update requirements

--- backend/VideoParse.py
+++ backend/VideoParse.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 def downloadYt(url, fname):
     yt = YouTube(url)
     video = yt.filter('mp4')[-1] # highest res
-    video.download('./videos/')
+    video.download(fname)
 
 if __name__ == '__main__':
     downloadYt('https://www.youtube.com/watch?v=m-QToZcV84Q', 'test.mp4')
