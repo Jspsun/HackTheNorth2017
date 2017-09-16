0747f2ade4c30f5a788b23b8811f22dbf81dbe4a
ZanderShah <alexander.shah@uwaterloo.ca>
2017-09-16 03:09:08 -0400

    ocr

---
+++ backend/OCR.py
@@ -0,0 +1,25 @@
+import cv2
+from PIL import Image
+import pytesseract
+
+def process_video(file):
+  vid = cv2.VideoCapture(file)
+
+  count = 0
+  ret = []
+  while True:
+    success, image = vid.read()
+    print('Reading a new frame ..', success)
+    cv2.imwrite('frame{}.jpg'.format(count), image)
+    ret += process_picture('frame{}.jpg'.format(count))
+    count += 1
+
+def process_picture(file):
+  return pytesseract.image_to_string(Image.open(file))
+
+if __name__ == '__main__':
+  print(process_picture('./test.png'))
+
+# pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+# Include the above line, if you don't have tesseract executable in your PATH
+# Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
