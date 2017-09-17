353525e5297fe12da65c7220118e174f6a2de4be
Jonathan Sun <jonathan.sun@thalmic.com>
2017-09-16 21:36:03 -0400

    added navbar

--- frontend/src/Navbar/NavBar.js
+++ frontend/src/Navbar/NavBar.js
@@ -2,19 +2,57 @@ import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 export class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({
+        query: e.target.value
+      })
+      console.log(e.target.value);
+    }
+  }
 
   render() {
     const styles = StyleSheet.create({
       wrapper: {
         display: 'flex',
-        backgroundColor: '#000',
-        color: '#F9CC9D'
+        padding: '30px 10px 0px 30px',
+        color: '#4E8BD9',
+        fontFamily: 'Baloo Tammudu',
+        lineHeight: '50px',
+        fontSize: '40px',
+        fontWeight: '100',
+        boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+        flexDirection: 'row'
+      },
+      search: {
+        marginLeft: 'Auto',
+        border: 'none',
+        borderRadius: '20px',
+        display: 'flex',
+        backgroundColor: '#F1F1F1',
+        height: '20px',
+        width: '500px',
+        padding: '15px',
+        marginTop: '-10px'
+      },
+      enter: {
+        // position: 'absolute'
       }
     });
 
     return (
       <div className={css(styles.wrapper)}>
-        sampleText
+        <div>
+          LearnR
+        </div>
+        <input className={css(styles.search)} onKeyPress={this.onKeyPress} id='search' type='text' placeholder='input youtube url'/>
       </div>
     );
   }
